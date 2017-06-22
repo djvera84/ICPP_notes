3c95eccfaabf8afe3a407229effde18eec176bad
Daniel J. Vera <djvera@gmail.com>
2017-06-22 07:43:27 -0700

    Add files via upload

--- ICPP_notes/ICPP_ch8.py
+++ ICPP_notes/ICPP_ch8.py
@@ -97,7 +97,7 @@ class Person(object):
         return self.last_name
     
     def set_birthday(self, birthdate):
-        """Assumes birthdaye is of type datetime.date
+        """Assumes birthday is of type datetime.date
            Sets self's birthday to birthdate"""
         self.birthday = birthdate
         
@@ -365,4 +365,93 @@ book = BetterGrades()
 book.add_student(Grad('Julie'))
 book.add_student(Grad('Charlie'))
 for s in book.better_get_students():
-    print(s)   
+    print(s)
+
+#%% Mortgages, an Extended Example =========================================
+def find_payment(loan, r, m):
+    """Assumes: loan and r are floats, m an int
+       Returns the monthly payment for a mortgage of size
+       loan at a monthly rate of r for m months"""
+    return loan * ((r * (1 + r)**m) / ((1 + r)**m - 1))
+
+# Abstract class is a class that contains methods shared by each subclass
+# but it not intended to be instantiated directly.
+class Mortgage(object):
+    """Abstract class for building different kinds of mortgages"""
+    def __init__(self, loan, ann_rate, months):
+        """Assumes: loan and ann_rate are floats, months an int
+           Creates a new mortgage of size loan, duration months, and
+           annual reate ann_rate"""
+        self.loan = loan
+        self.rate = ann_rate / 12
+        self.months = months
+        self.paid = [0.0]
+        self.outstanding = [loan]
+        self.payment = find_payment(loan, self.rate, months)
+        self.legend = None # description of mortgage.
+    
+    def make_payment(self):
+        """Make a payment"""
+        self.paid.append(self.payment)
+        reduction = self.payment - self.outstanding[-1] * self.rate
+        self.outstanding.append(self.outstanding[-1] - reduction)
+    
+    def get_total_paid(self):
+        """Return the total amount paid so far"""
+        return sum(self.paid)
+    
+    def __str__(self):
+        return self.legend
+
+
+class Fixed(Mortgage):
+    def __init__(self, loan, r, months):
+        Mortgage.__init__(self, loan, r, months)
+        self.legend = 'Fixed, ' + str(round(r*100, 2)) + '%'
+
+
+class FixedWithPts(Mortgage):
+    def __init__(self, loan, r, months, pts):
+        Mortgage.__init__(self, loan, r, months)
+        self.pts = pts
+        self.paid = [loan * (pts/100)]
+        self.legend = 'Fixed, ' + str(round(r*100, 2)) + '%, '\
+                      + str(pts) + ' points'
+
+
+class TwoRate(Mortgage):
+    def __init__(self, loan, r, months, teaser_rate, teaser_months):
+        Mortgage.__init__(self, loan, teaser_rate, months)
+        self.teaser_months = teaser_months
+        self.teaser_rate = teaser_rate
+        self.next_rate = r / 12
+        self.legend = str(teaser_rate * 100)\
+                     + '% for ' + str(self.teaser_months)\
+                     + ' months, then ' + str(round(r*100, 2)) + '%'
+
+    def make_payment(self):
+        if len(self.paid) == self.teaser_months + 1:
+            self.rate = self.next_rate
+            self.payment = find_payment(self.outstanding[-1],
+                                        self.rate,
+                                        self.months - self.teaser_months)
+        Mortgage.make_payment(self)
+
+
+def compare_mortgages(amt, years, fixed_rate, pts, pts_rate, 
+                      var_rate1, var_rate2, var_months):
+    tot_months = years * 12
+    fixed1 = Fixed(amt, fixed_rate, tot_months)
+    fixed2 = FixedWithPts(amt, pts_rate, tot_months, pts)
+    two_rate = TwoRate(amt, var_rate2, tot_months, var_rate1, var_months)
+    morts = [fixed1, fixed2, two_rate]
+    for m in range(tot_months):
+        for mort in morts:
+            mort.make_payment()
+    for m in morts:
+        print(m)
+        print(' Total payments = $' + str(int(m.get_total_paid())))
+
+compare_mortgages(amt=200000, years=30, fixed_rate=0.07,
+                  pts=3.25, pts_rate=0.05, var_rate1=0.045,
+                  var_rate2=0.095, var_months=48)
