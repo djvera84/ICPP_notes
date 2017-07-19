a22cacb18cdda92fbebd11ef127d52396b886fab
Daniel J. Vera <djvera@gmail.com>
2017-07-18 18:27:53 -0700

    Add files via upload

--- ICPP_notes/ICPP_ch15.py
+++ ICPP_notes/ICPP_ch15.py
@@ -321,4 +321,251 @@ flip_plot2(4, 20, 20)
 #%%
 flip_plot2(4, 20, 1000) #takes a while to run, ~ 20 min.
 
-#%% Distributions ========================================================== 
+#%% Distributions ==========================================================
+def flip_for_sim(num_flips):
+    """Assumes num_flips a positive int"""
+    heads = 0
+    for i in range(num_flips):
+        if random.choice(('H', 'T')) == 'H':
+            heads += 1
+    return heads / float(num_flips)
+
+def flip_sim(num_flips_per_trial, num_trials):
+    frac_heads = []
+    for i in range(num_trials):
+        frac_heads.append(flip_for_sim(num_flips_per_trial))
+    mean = sum(frac_heads) / len(frac_heads)
+    sd = std_dev(frac_heads)
+    return (frac_heads, mean, sd)
+
+def label_plot(num_flips, num_trials, mean, sd):
+    pylab.title(str(num_trials) + ' trials of'
+                + str(num_flips) + 'flips each')
+    pylab.xlabel('Fraction of Heads')
+    pylab.ylabel('Number of Trials')
+    pylab.annotate('Mean =' + str(round(mean, 4))\
+                   + '\nSD = ' + str(round(sd, 4)), size='x-large',
+                   xycoords = 'axes fraction', xy = (0.67, 0.5))
+
+def make_plots(num_flips1, num_flips2, num_trials):
+    val1, mean1, sd1 = flip_sim(num_flips1, num_trials)
+    pylab.hist(val1, bins = 20)
+    xmin, xmax = pylab.xlim()
+    label_plot(num_flips1, num_trials, mean1, sd1)
+    pylab.figure()
+    val2, mean2, sd2 = flip_sim(num_flips2, num_trials)
+    pylab.hist(val2, bins = 20)
+    pylab.xlim(xmin, xmax)
+    label_plot(num_flips2, num_trials, mean2, sd2)
+
+make_plots(100, 1000, 100000)
+
+#%% Normal Distributions
+import scipy.integrate
+
+
+def gaussian(x, mu, sigma):
+    factor1 = (1.0 / (sigma * ((2 * pylab.pi)**0.5)))
+    factor2 = pylab.e**-(((x - mu)**2) / (2 * sigma**2))
+    return factor1 * factor2
+
+def check_empirical(num_trials):
+    for t in range(num_trials):
+        mu = random.randint(-10, 10)
+        sigma = random.randint(1, 10)
+        print('For mu =', mu, 'and sigma =', sigma)
+        for num_std in (1, 2, 3):
+            area = scipy.integrate.quad(gaussian, mu - num_std * sigma,
+                                                  mu + num_std * sigma,
+                                        (mu, sigma)) [0]
+            print('     Fraction within', num_std, 'std =',
+                  round(area, 4))
+
+check_empirical(3)
+#%%
+def show_error_bars(min_exp, max_exp, num_trials):
+    """Assumes min_exp and max_exp positive ints; min_exp < max_exp
+       num_trials a positive integer
+       Plots mean fraction of heads with error bars"""
+    means, sds, x_vals = [], [], []
+    for exp in range(min_exp, max_exp + 1):
+        x_vals.append(2**exp)
+        frac_heads, mean, sd = flip_sim(2**exp, num_trials)
+        means.append(mean)
+        sds.append(sd)
+    pylab.errorbar(x_vals, means, yerr=1.96*pylab.array(sds))
+    pylab.semilogx()
+    pylab.title('Mean Fraction of Heads('
+                + str(num_trials) + ' trials)')
+    pylab.xlabel('Number of flips per trial')
+    pylab.ylabel('Fraction of heads & 95% confidence')
+        
+show_error_bars(3, 10, 100)
+
+#%% Continuous and Discrete Uniform Distributions
+
+# use random.uniform(min, max) for continuous
+# use random.randint(min, max) for discrete
+
+#%% Binomial and Multinomial Distributions
+# Finger Exercise: Implement a function that calculates the probability of
+# rolling exactly two 3’s in k rolls of a fair die. Use this function to 
+# plot the probability as k varies from 2 to 100.
+import math
+# Probability of rolling one 3 in one roll is 1 / 6 for fair die. Therefore,
+# in k trials the probability of rolling exactly 2 rolls is 
+# (k choose 2) * (1/6)**2 * (5/6)**(k-2)
+
+def choose(n, k):
+    """Assumes n, k >= 0 integers and n > k"""
+    return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+
+def binomial_prob(n, k, p=1/6):
+    """Assumes n, k >= 0 integers and n > k,
+       p a float between 0 and 1, default 1/6, (think dice) 
+       Returns probability of rolling exactly k rolls in n trials"""
+    binomial = choose(n, k)
+    successes = p**k
+    failures = (1 - p)**(n-k)
+    return binomial * successes * failures
+
+def plot_binom_probs(min_trial = 2, max_trial = 100):
+    x_vals = []
+    probs = []
+    for i in range(min_trial, max_trial + 1):
+        x_vals.append(i)
+    for x in x_vals:
+        probs.append(binomial_prob(x, 2))
+    pylab.figure()
+    pylab.plot(x_vals, probs, 'b|')
+    pylab.xlabel('Number of Trials')
+    pylab.ylabel('Probability of Exactly 2 Successes in x trials')
+
+plot_binom_probs()
+#%% Exponential and Geometric Distributions
+def clear(n, p, steps):
+    """Assumes n & steps positive ints, p a float
+       n: the initial number of molecules
+       p: the probability of molecules being cleared
+       steps: the length of the simulation"""
+    num_remaining = [n]
+    for t in range(steps):
+        num_remaining.append(n*((1-p)**t))
+    pylab.plot(num_remaining)
+    pylab.xlabel('Time')
+    pylab.ylabel('Molecules Remaining')
+    pylab.title('Clearance of Drug')
+
+clear(1000, 0.01, 1000)
+
+def clear_log_scale(n, p, steps):
+    """Assumes n & steps positive ints, p a float
+       n: the initial number of molecules
+       p: the probability of molecules being cleared
+       steps: the length of the simulation"""
+    num_remaining = [n]
+    for t in range(steps):
+        num_remaining.append(n*((1-p)**t))
+    pylab.plot(num_remaining)
+    pylab.xlabel('Time')
+    pylab.ylabel('Molecules Remaining')
+    pylab.semilogy()
+    pylab.title('Clearance of Drug')
+
+clear_log_scale(1000, 0.01, 1000)
+
+# random.expovariate(lambd) gives exponential distribution with mean 1/lambd
+# Geometric distribution is discrete analog of exponential distribution.
+#%%
+def successful_starts(success_prob, num_trials):
+    """Assumes success_prob is a float representing probaiblity of a 
+       single attempt being successful. num_trials a positive int.
+       Returns a list of the number of attempts needed before a
+       success for each trial"""
+    tries_before_success = []
+    for t in range(num_trials):
+        consec_failures = 0
+        while random.random() > success_prob:
+            consec_failures += 1
+        tries_before_success.append(consec_failures)
+    return tries_before_success
+
+prob_of_success = 0.5
+num_trials = 5000
+distribution = successful_starts(prob_of_success, num_trials)
+pylab.hist(distribution, bins = 14)
+pylab.xlabel('Tries Before Success')
+pylab.ylabel('Number of Occurences Out of ' + str(num_trials))
+pylab.title('Probability of Starting Each Try = '\
+            + str(prob_of_success))
+
+#%% Benford's Distribution
+# A set of decimal numbers is said to satisfy Benford’s law107 if the 
+# probability of the first digit being d is consistent with 
+# P(d) = log10(1 + 1/ d).
+
+#%% Hashing and Collisions =================================================
+def collision_prob(n, k):
+    prob = 1.0
+    for i in range(1, k):
+        prob = prob * ((n-i) / n)
+    return 1 - prob
+
+collision_prob(1000, 50)
+collision_prob(1000, 200)
+
+def sim_insertions(num_indices, num_insertions):
+    """Assumes num_indices and num_insertions are positive ints.
+       Returns 1 if there is a collision; 0 otherwise"""
+    choices = range(num_indices) # list of possible indices
+    used = []
+    for i in range(num_insertions):
+        hash_val = random.choice(choices)
+        if hash_val in used: # there is a collision
+            return 1
+        else:
+            used.append(hash_val)
+    return 0
+
+def find_prob(num_indices, num_insertions, num_trials):
+    collisions = 0
+    for t in range(num_trials):
+        collisions += sim_insertions(num_indices, num_insertions)
+    return collisions / num_trials
+
+print('Actual probability of a collision =', collision_prob(1000, 50))
+print('Est. probability of a collision =', find_prob(1000, 50, 10000))
+print('Actual probability of a collision =', collision_prob(1000, 200))
+print('Est. probability of a collision =', find_prob(1000, 200, 10000))
+
+#%% How Often Does the Better Team Win?
+
+def play_series(num_games, team_prob):
+    num_won = 0
+    for game in range(num_games):
+        if random.random() <= team_prob:
+            num_won += 1
+    return (num_won > num_games//2)
+
+def fraction_won(team_prob, num_series, series_len):
+    won = 0
+    for series in range(num_series):
+        if play_series(series_len, team_prob):
+            won += 1
+    return won/float(num_series)
+
+def sim_series(num_series):
+    prob = 0.5
+    fracs_won, probs = [], []
+    while prob <= 1.0:
+        fracs_won.append(fraction_won(prob, num_series, 7))
+        probs.append(prob)
+        prob += 0.01
+    pylab.axhline(0.95) # Draw a line at 95%
+    pylab.plot(probs, fracs_won, 'k', linewidth = 5)
+    pylab.xlabel('Probability of Winning a Game')
+    pylab.ylabel('Probability of Winning a Series')
+    pylab.title(str(num_series) + ' Seven-Game Series')
+
+sim_series(400)
+
