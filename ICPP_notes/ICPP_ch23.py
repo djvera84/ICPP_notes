dbf1fa9468cd31cf4fb116a195ecb0e9e2ca25e1
Daniel J. Vera <djvera@gmail.com>
2017-08-04 10:04:33 -0700

    Add files via upload

--- ICPP_notes/ICPP_ch23.py
+++ ICPP_notes/ICPP_ch23.py
@@ -6,6 +6,7 @@ Clustering
 """
 import pylab
 import random
+import numpy as np
 #%% Class Cluster ==========================================================
 def minkowski_dist(v1, v2, p):
     """Assumes v1 and v2 are equal-length arrays of numbers
@@ -230,3 +231,150 @@ contrived_test2(40, 3)
 #%%
 contrived_test2(40, 6)
 #%% A Less Contrived Example ===============================================
+def read_mammal_data(fname):
+    data_file = open(fname, 'r')
+    num_features = 0
+    # Process lines at top of file
+    for line in data_file: # Find number of features
+        if line[0:6] == '#Label': # indicates end of features
+            break
+        if line[0:5] != '#Name':
+            num_features += 1
+    
+    # Produce feature_vals, species_names, and label_list
+    feature_vals, species_names, label_list = [], [], []
+    for i in range(num_features):
+        feature_vals.append([])
+    
+    # Continue processing lines in file, starting after comments
+    for line in data_file:
+        # remove newline, then split
+        data_line = line[:-1].split(',')
+        species_names.append(data_line[0])
+        class_label = data_line[-1]
+        label_list.append(class_label)
+        for i in range(num_features):
+            feature_vals[i].append(float(data_line[i+1]))
+    
+    # Use feature_vals to build list containing the feature vectors
+    # for each mammal
+    feature_vector_list = []
+    for mammal in range(len(species_names)):
+        feature_vector = []
+        for feature in range(num_features):
+            feature_vector.append(feature_vals[feature][mammal])
+        feature_vector_list.append(feature_vector)
+    return feature_vector_list, label_list, species_names
+
+def build_mammal_examples(feature_list, label_list, species_names):
+    examples = []
+    for i in range(len(species_names)):
+        features = pylab.array(feature_list[i])
+        example = Example(species_names[i], features, label_list[i])
+        examples.append(example)
+    return examples
+
+def test_teeth(num_clusters, num_trials):
+    features, labels, species = read_mammal_data('dentalFormulas.txt')
+    examples = build_mammal_examples(features, labels, species)
+    best_clustering = try_kmeans(examples, num_clusters, num_trials)
+    for c in best_clustering:
+        names = ''
+        for p in c.members():
+            names += p.get_name() + ', '
+        print('\n' + names[:-2]) # remove trailing comma and space
+        herbivores, carnivores, omnivores = 0, 0, 0
+        for p in c.members():
+            if p.get_label() == '0':
+                herbivores += 1
+            elif p.get_label() == '1':
+                carnivores += 1
+            else:
+                omnivores += 1
+        print(herbivores, 'herbivores,', carnivores, 'carnivores,',
+              omnivores, 'omnivores')
+
+test_teeth(3, 40)
+#%%
+def z_scale_features(vals):
+    """Assumes vals is a sequence of floats"""
+    result = pylab.array(vals)
+    mean = sum(result) / len(result)
+    result = result - mean
+    return result / np.std(result)
+
+def i_scale_features(vals):
+    """Assumes vals is a sequence of floats"""
+    min_val, max_val = min(vals), max(vals)
+    fit = pylab.polyfit([min_val, max_val], [0, 1], 1)
+    return pylab.polyval(fit, vals)
+#%%
+def read_mammal_data_scale(fname, scale):
+    data_file = open(fname, 'r')
+    num_features = 0
+    # Process lines at top of file
+    for line in data_file: # Find number of features
+        if line[0:6] == '#Label': # indicates end of features
+            break
+        if line[0:5] != '#Name':
+            num_features += 1
+    
+    # Produce feature_vals, species_names, and label_list
+    feature_vals, species_names, label_list = [], [], []
+    for i in range(num_features):
+        feature_vals.append([])
+    
+    # Continue processing lines in file, starting after comments
+    for line in data_file:
+        # remove newline, then split
+        data_line = line[:-1].split(',')
+        species_names.append(data_line[0])
+        class_label = data_line[-1]
+        label_list.append(class_label)
+        for i in range(num_features):
+            feature_vals[i].append(float(data_line[i+1]))
+    
+    # Use feature_vals to build list containing the feature vectors
+    # for each mammal
+    for i in range(num_features):
+        feature_vals[i] = scale(feature_vals[i])
+    feature_vector_list = []
+    for mammal in range(len(species_names)):
+        feature_vector = []
+        for feature in range(num_features):
+            feature_vector.append(feature_vals[feature][mammal])
+        feature_vector_list.append(feature_vector)
+    return feature_vector_list, label_list, species_names
+
+def test_teeth_scale(num_clusters, num_trials, scale = lambda x: x):
+    features, labels, species =\
+              read_mammal_data_scale('dentalFormulas.txt', scale)
+    examples = build_mammal_examples(features, labels, species)
+    
+    ###Remainder of test_teeth is the same as above.###
+    best_clustering = try_kmeans(examples, num_clusters, num_trials)
+    for c in best_clustering:
+        names = ''
+        for p in c.members():
+            names += p.get_name() + ', '
+        print('\n' + names[:-2]) # remove trailing comma and space
+        herbivores, carnivores, omnivores = 0, 0, 0
+        for p in c.members():
+            if p.get_label() == '0':
+                herbivores += 1
+            elif p.get_label() == '1':
+                carnivores += 1
+            else:
+                omnivores += 1
+        print(herbivores, 'herbivores,', carnivores, 'carnivores,',
+              omnivores, 'omnivores')
+#%%
+random.seed(0) # so two clusterings starts with same seed
+print('Clustering without scaling')
+test_teeth_scale(3, 40)
+random.seed(0) # so two clusterings starts with same seed
+print('\nClustering with z-scaling')
+test_teeth_scale(3, 40, z_scale_features)
+random.seed(0) # so two clusterings starts with same seed
+print('\nClustering with i-scaling')
+test_teeth_scale(3, 40, i_scale_features)
